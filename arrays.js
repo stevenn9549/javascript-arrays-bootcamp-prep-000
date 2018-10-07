7a8fdb2628e6f6de1766fcb90ede9ab3ffd920e1
steven real sanchez <stevenn9549@gmail.com>
2018-10-07 02:18:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,3 +17,6 @@ function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
   return array;
 }
+function accessElementInArray(array, index){
+  return console.log(accessElementInArray[1]);
+}
