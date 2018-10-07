c2e4fa0d2049507384c9f4e6434a8b761b7ea826
steven real sanchez <stevenn9549@gmail.com>
2018-10-07 01:40:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
- return 
+ return  [elemnt, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(){
   
